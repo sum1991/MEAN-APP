48395b6797e2b0c412a646dcb7202d065bf0c8f3
Sumukh Lagadamane Shivashankara <ls.sumukh@gmail.com>
2016-10-01 17:59:22 -0700

    final implementation

--- app/routes.js
+++ app/routes.js
@@ -30,4 +30,66 @@ module.exports =function (app) {
         });
     });
 
+
+    // Retrieves JSON records for all users who meet a certain set of query conditions
+    app.post('/query/', function(req, res){
+
+        // Grab all of the query parameters from the body.
+        var lat             = req.body.latitude;
+        var long            = req.body.longitude;
+        var distance        = req.body.distance;
+        var male            = req.body.male;
+        var female          = req.body.female;
+        var other           = req.body.other;
+        var minAge          = req.body.minAge;
+        var maxAge          = req.body.maxAge;
+        var favLang         = req.body.favlang;
+        var reqVerified     = req.body.reqVerified;
+
+        // Opens a generic Mongoose Query. Depending on the post body we will...
+        var query = User.find({});
+
+        // ...include filter by Max Distance (converting miles to meters)
+        if(distance){
+
+            // Using MongoDB's geospatial querying features. (Note how coordinates are set [long, lat]
+            query = query.where('location').near({ center: {type: 'Point', coordinates: [long, lat]},
+
+                // Converting meters to miles. Specifying spherical geometry (for globe)
+                maxDistance: distance * 1609.34, spherical: true});
+        }
+
+        if(male || female || other){
+            query.or([{ 'gender': male }, { 'gender': female }, {'gender': other}]);
+        }
+
+        // ...include filter by Min Age
+        if(minAge){
+            query = query.where('age').gte(minAge);
+        }
+
+        // ...include filter by Max Age
+        if(maxAge){
+            query = query.where('age').lte(maxAge);
+        }
+
+        // ...include filter by Favorite Language
+        if(favLang){
+            query = query.where('favlang').equals(favLang);
+        }
+
+        // ...include filter for HTML5 Verified Locations
+        if(reqVerified){
+            query = query.where('htmlverified').equals("Yep (Thanks for giving us real data!)");
+        }
+
+        // Execute Query and Return the Query Results
+        query.exec(function(err, users){
+            if(err)
+                res.send(err);
+
+            // If no errors, respond with a JSON of all users that meet the criteria
+            res.json(users);
+        });
+    });
 };
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,18 @@
-/**
- * Created by sumukh on 9/29/16.
- */
+var app = angular.module('meanMapApp', ['addCtrl', 'queryCtrl', 'geolocation', 'gservice', 'ngRoute'])
+
+// Configures Angular routing -- showing the relevant view and controller when needed.
+    .config(function($routeProvider){
+
+        // Join Team Control Panel
+        $routeProvider.when('/join', {
+            controller: 'addCtrl',
+            templateUrl: 'partials/addForm.html',
+
+            // Find Teammates Control Panel
+        }).when('/find', {
+            controller: 'queryCtrl',
+            templateUrl: 'partials/queryForm.html',
+
+            // All else forward to the Join Team Control Panel
+        }).otherwise({redirectTo:'/join'})
+    });
